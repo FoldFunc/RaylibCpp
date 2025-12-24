4ff84b49dbf5b8610b04b63f9a2c2d9b96c55b2e
FoldFunc <fold.func@gmail.com>
2025-12-24 11:20:34 +0000

    Changing the Background color.

--- src/engine/app.cpp
+++ src/engine/app.cpp
@@ -1,5 +1,11 @@
+// The run method will every frame check the list of methods that should be
+// on the screen.
+//
+// I am a genius.
+
 #include "raylib.h"
 #include "app.hpp"
+
 App::App() = default;
 bool App::build(const int sW, const int sH, const std::string &name) {
   screenName = name;
@@ -13,10 +19,24 @@ bool App::build(const int sW, const int sH, const std::string &name) {
   SetTargetFPS(60);
   return true;
 }
+void App::EngBGColor(const EngColor c) {
+  comms.push_back([this, c] () { BGColor(c); });
+}
+void App::BGColor(const EngColor c) {
+  BeginDrawing();
+  Color nc;
+  nc.r = static_cast<unsigned char>(c.r * 255);
+  nc.g = static_cast<unsigned char>(c.g * 255);
+  nc.b = static_cast<unsigned char>(c.b * 255);
+  nc.a = 255;
+  ClearBackground(nc);
+  EndDrawing();
+}
 void App::run() {
   while (!WindowShouldClose()) {
-    continue;
+    for (auto &cmd : comms) {
+      cmd();
+    }
   }
-
   CloseWindow();
 }
--- src/engine/app.hpp
+++ src/engine/app.hpp
@@ -1,13 +1,33 @@
 #pragma once
+#include <functional>
 #include <string>
+#include <vector>
+
+
+struct EngColor {
+  float r, g, b;
+};
+
+constexpr EngColor ENGWHITE = {1.0f, 1.0f, 1.0f};
+
 class App {
+
 public:
   App();
 
   bool build(const int sW, const int sH, const std::string &name);
   void run();
+
+  void EngBGColor(const EngColor c);
+  void BGColor(const EngColor c);
+
+
 private:
   int screenWidth = 0;
   int screenHeight = 0;
   std::string screenName;
+
+  std::vector<std::function<void()>> comms;
+
 };
+
