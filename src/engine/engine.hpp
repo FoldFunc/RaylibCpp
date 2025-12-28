84e41cf4946ee1194d3eb3cb1fe749e325374c5c
FoldFunc <fold.func@gmail.com>
2025-12-28 08:14:00 +0000

    I lost the bet, the code I wrote yesterday was infact not shit. Wave function collapse for generating terraind checked.

--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -19,8 +19,17 @@ enum class ENGKeys {
 
 struct EngColor {
   float r, g, b;
+  auto operator<=>(const EngColor&) const = default;
 };
 
-constexpr EngColor ENGWHITE = {1.0f, 1.0f, 1.0f};
-constexpr EngColor ENGBLACK = {0.0f, 0.0f, 0.0f};
-constexpr EngColor ENGRED   = {1.0f, 0.0f, 0.0f};
+constexpr EngColor ENGWHITE   = {1.0f, 1.0f, 1.0f};
+constexpr EngColor ENGBLACK   = {0.0f, 0.0f, 0.0f};
+constexpr EngColor ENGRED     = {1.0f, 0.0f, 0.0f};
+constexpr EngColor ENGBLUE    = {0.0f, 0.0f, 1.0f};
+constexpr EngColor ENGGREEN   = {0.0f, 1.0f, 0.0f};
+constexpr EngColor ENGMAGENTA = {1.0f, 0.0f, 1.0f};
+
+constexpr EngColor ENGGRAY    = {128.0f/255.0f, 128.0f/255.0f, 128.0f/255.0f};
+constexpr EngColor ENGGRASS   = {76.0f/255.0f, 175.0f/255.0f, 80.0f/255.0f};
+constexpr EngColor ENGSAND    = {237.0f/255.0f, 201.0f/255.0f, 175.0f/255.0f};
+constexpr EngColor ENGWATER   = {64.0f/255.0f, 164.0f/255.0f, 223.0f/255.0f};
